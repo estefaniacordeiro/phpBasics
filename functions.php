68a142f99f1965a496d4ea18ac4557739e1bd263
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 23:57:14 +0100

    Function returns division of two numbers

--- functions.php
+++ functions.php
@@ -14,4 +14,11 @@ function mult($num1, $num2){
 echo "12 * 2 = " . mult(12,2);
 echo '<br>';
 
+//It generates a function that given two numbers returns the division of both
+function div($num1, $num2){
+    return $num1 / $num2;
+}
+echo "11 / 2 = ".div(11,2);
+echo '<br>';
+
 ?>

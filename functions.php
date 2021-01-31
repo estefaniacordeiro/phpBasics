612e02204fdf4e1c0ce5a6d919fbadd1cf6ca4b2
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 23:55:07 +0100

    Function returns sum of two numbers

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+//It generates a function that given two numbers returns the sum of both
+function sum($num1, $num2){
+    return $num1 + $num2;
+}
+echo "1 + 2 = " . sum(1,2);
+echo '<br>';
+
+
+?>

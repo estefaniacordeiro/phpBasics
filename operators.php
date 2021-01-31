66b293c0e12ae564b130f05f34dce36e7d4daed6
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 21:55:15 +0100

    Create an example of use for arithmetic operators: +, -, *, /, and %

---
+++ operators.php
@@ -0,0 +1,20 @@
+<?php
+
+//Create an example of use for arithmetic operators: +, -, *, /, and%
+    $a = 5;
+    $b = 10;
+    echo '<br>';
+    echo $a + $b;
+    echo '<br>';
+    echo $a - $b;
+    echo '<br>';
+    echo $a * $b;
+    echo '<br>';
+    echo $a / $b;
+    echo '<br>';
+    echo $a % $b;
+    echo '<br>';
+
+
+
+?>

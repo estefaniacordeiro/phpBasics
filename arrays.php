c3681cb0794d41a5710408354d70ba463b3ab085
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 23:40:43 +0100

    Define a simple array of numbers and decimal numbers

--- arrays.php
+++ arrays.php
@@ -5,4 +5,9 @@ $arr= array('one', 'two', 'three', 'four', 'five');
 print_r($arr);
 echo '<br>';
 
+//Define a simple array consisting of whole numbers and decimal numbers
+$arrNum = array(16, 3.2 , -4);
+print_r($arrNum);
+echo '<br>';
+
 ?>

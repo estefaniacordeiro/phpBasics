3225669bc84052f1453759ef9ad2742a279d35f9
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 22:08:53 +0100

    Get the current minute with leading zeros (00 - 59)

--- dates.php
+++ dates.php
@@ -1,9 +1,10 @@
 <?php
 
 //Instance the Date Time class and then invoke the format method with the argument “Y-m-d” to show year-month-day
-    $yearMonthDay = date('Y-m-d');
-    echo $yearMonthDay;
-    echo '<br>';
+$yearMonthDay = date('Y-m-d');
+echo '<br>';
+echo $yearMonthDay;
+echo '<br>';
 
 //Get the current date in any format
 echo date('d-m-Y');
@@ -16,5 +17,8 @@ echo '<br>';
 //Get the current month in numerical format (1-12)
 echo date('m');
 echo '<br>';
+
+//Get the current minute with leading zeros (00 - 59)
+echo date('i');
 
 ?>

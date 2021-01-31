bb04a7f5650fea345aad8030f93371b81c6b1336
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 23:17:09 +0100

    Function that returns the highest value of a series

--- maths.php
+++ maths.php
@@ -12,4 +12,10 @@ $round = 3.5679;
 echo round($round);
 echo '<br>';
 
+
+//Define a variable whose value is the result of the function that returns the highest value of a series of values ​​that are received by parameter.
+$arr = array(5.5, 3, 20, -15, 0);
+echo max($arr);
+echo '<br>';
+
 ?>

f5875d9f86209c521a865b600204d6405ce7fe4c
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 23:15:43 +0100

    Function that returns a rounded value to highest integer

--- maths.php
+++ maths.php
@@ -1,10 +1,15 @@
 <?php
 
+echo '<br>';
+
 //Define a variable whose value is the result of the function that returns an absolute value.
 $value = -6;
 echo abs($value);
 echo '<br>';
 
-
+//Define a variable whose value is the result of the function that returns a rounded value to the next highest integer.
+$round = 3.5679;
+echo round($round);
+echo '<br>';
 
 ?>

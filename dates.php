41b635fc8f638d668c0c9275403e5b0b48248ecb
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 22:04:43 +0100

    Instance the Date Time class with format Y-m-d

---
+++ dates.php
@@ -0,0 +1,9 @@
+<?php
+
+//Instance the Date Time class and then invoke the format method with the argument “Y-m-d” to show year-month-day
+    $yearMonthDay = date('Y-m-d');
+    echo $yearMonthDay;
+    echo '<br>';
+
+
+?>

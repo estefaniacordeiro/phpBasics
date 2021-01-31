2ba858b70157815ab0e99544435fdd7b2ee22206
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2021-01-31 23:56:20 +0100

    Function returns multiplication of two numbers

--- functions.php
+++ functions.php
@@ -7,5 +7,11 @@ function sum($num1, $num2){
 echo "1 + 2 = " . sum(1,2);
 echo '<br>';
 
+//It generates a function that given two numbers returns the multiplication of both
+function mult($num1, $num2){
+    return $num1 * $num2;
+}
+echo "12 * 2 = " . mult(12,2);
+echo '<br>';
 
 ?>
